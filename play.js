1d380112da6b2e4e1d7fe3fa24ce2f3d7b75e784
Patrik Suhaj <suhajpato@gmail.com>
2023-11-16 22:40:56 +0100

    async code with promise

--- play.js
+++ play.js
@@ -1,14 +1,22 @@
-const fetchData = callback => {
-    setTimeout(() => {
-        callback('done');
-    }, 1500);
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('done');
+        }, 1500);
+    });
+    return promise;
 }
 
 setTimeout(() => {
     console.log('timer is done');
-    fetchData(text => {
-        console.log(text + 1);
-    })
+    fetchData()
+        .then(text => {
+            console.log(text);
+            return fetchData();
+        })
+        .then(text2 => {
+            console.log(text2);
+        });
 }, 2000);
 
 console.log('motha');
