00dea3a99f9836802e39a594a63d983e60d45641
Patrik Suhaj <suhajpato@gmail.com>
2023-11-16 22:43:30 +0100

    template literals

--- play.js
+++ play.js
@@ -1,22 +1,3 @@
-const fetchData = () => {
-    const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('done');
-        }, 1500);
-    });
-    return promise;
-}
-
-setTimeout(() => {
-    console.log('timer is done');
-    fetchData()
-        .then(text => {
-            console.log(text);
-            return fetchData();
-        })
-        .then(text2 => {
-            console.log(text2);
-        });
-}, 2000);
-
-console.log('motha');
+const name = 'Max';
+let age = 5;
+console.log(`kokot ${name} ma len ${age} rokov`);
